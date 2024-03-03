28af07ae9745aa7982190c25783c66d6f1fc8fca
stef-ania <stebofof@gmail.com>
2024-03-03 17:09:09 +0100

    Removed register form from homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,7 @@
-import RegisterUserForm from "./components/RegisterUserForm/RegisterUserForm.jsx";
-import Input from "./components/Input/Input.jsx";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hola Fefy</h1>
-      <RegisterUserForm></RegisterUserForm>
+      <h1>Hola girls :D</h1>
     </main>
   );
 }
