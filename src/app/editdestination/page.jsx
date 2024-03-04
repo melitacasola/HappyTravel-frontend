c71b93045d682d6d68437e93e513e9f6b6c7d36d
tephyxp <lindacespedesg@gmail.com>
2024-03-04 16:35:13 +0100

    edit destination page added

--- src/app/editdestination/page.jsx
+++ src/app/editdestination/page.jsx
@@ -9,23 +9,23 @@ export default function NewDestination() {
       {/* desktop version */}
       <div className="hidden md:flex items-center justify-center h-screen w-screen bg-white p-4 m-0">
         <div className="flex items-center justify-center w-3/5 h-92 border-[#fbfdce] rounded-b-xl px-5 py-7 m-2 p-3">
-          <NewDestinationForm title="Crear Destino"/>
+          <NewDestinationForm title="Editar Destino" />
         </div>
       </div>
-      
+
       {/* mobile version */}
       <div className='p-6 md:hidden'>
-  <div className=""> {/* Oculta en pantallas medianas y grandes */}
-    <div>
-      <div className="flex justify-center items-center">
-        <div className="mb-6 h-auto">
-          <LogoMobile />
+        <div className=""> {/* Oculta en pantallas medianas y grandes */}
+          <div>
+            <div className="flex justify-center items-center">
+              <div className="mb-6 h-auto">
+                <LogoMobile />
+              </div>
+            </div>
+            <NewDestinationMobile title="Editar Destino" />
+          </div>
         </div>
       </div>
-      <NewDestinationMobile title="Crear Destino"/>
-    </div>
-  </div>
-</div>
 
     </>
   )
