23985deb079bfaaee45a3a82e22b6a1fa29f6c7d
stef-ania <stebofof@gmail.com>
2024-03-06 12:22:53 +0100

    Added axios and connected destinations

--- src/app/components/RegisterUserForm/RegisterUserForm.jsx
+++ src/app/components/RegisterUserForm/RegisterUserForm.jsx
@@ -1,22 +1,39 @@
 "use client";
 
+import React, { useState } from "react";
+import { useFetchApi } from "../../services/useFetchApi";
+import axios from "../../services/axios";
+
 import Input from "../Input/Input.jsx";
 import Button from "../Button/Button.jsx";
 import Link from "next/link";
 
 const Form = () => {
+  const [register, setRegister] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const { data, loading, error } = useFetchApi("http://localhost:8000/api/register", "POST", register);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRegister((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+
+    console.log(register);
+  };
+
+  /*  */
   const registerWrapper = `border-4 rounded-3xl border-bg-color px-8 py-4 mx-auto my-20 max-w-sm`;
-
   const titleRegister = `text-2xl text-center text-secondary border-b border-secondary pb-2 mb-8`;
-
-  const formWrapper = `flex flex-col gap-1`;
-
+  const formWrapper = `flex flex-col gap-1 items-center`;
   const labelStyle = `text-text-color text-xl`;
-
   const buttonsWrapper = `flex flex-row place-content-evenly items-center mb-4`;
-
   const textStyle = `text-text-color text-xl text-center`;
-
   const linkStyle = `text-primary text-xl`;
 
   return (
@@ -26,20 +43,30 @@ const Form = () => {
         <label className={labelStyle}>Nombre</label>
         <Input
           type="text"
+          name="name"
           placeholder="Escribe tu nombre ..."
           pattern="[A-Za-z ]+"
           validationMessage="Nombre requerido"
           required
+          onChange={handleChange}
         />
         <label className={labelStyle}>E-mail</label>
         <Input
           type="email"
+          name="email"
           placeholder="Escribe tu e-mail ..."
           pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
           required
+          onChange={handleChange}
         />
         <label className={labelStyle}>Contraseña</label>
-        <Input type="password" placeholder="Escribe tu contraseña ..." required />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Escribe tu contraseña ..."
+          required
+          onChange={handleChange}
+        />
         <div className={buttonsWrapper}>
           <Button type="submit" text="Aceptar" isPrimary={true} />
           <Button text="Cancelar" isPrimary={false} />
---
+++ src/app/services/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const urlAPI = "http://localhost:8000/api";
+
+export const getDestinations = async () => {
+  try {
+    const response = await axios.get(`${urlAPI}/destinations`);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
