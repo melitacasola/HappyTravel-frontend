c0244bae2c8a76604c25edc587e6eab1bcc03e53
stef-ania <stebofof@gmail.com>
2024-03-07 15:54:33 +0100

    Added info-icon in destination card

--- src/app/components/DestinationCard/DestinationCard.jsx
+++ src/app/components/DestinationCard/DestinationCard.jsx
@@ -6,6 +6,7 @@ const DestinationCard = ({ data }) => {
   const gridWrapper = `grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[2rem]`;
   const gridItem = `w-[18.75rem] bg-bg-color rounded-[20px_20px_20px_20px]`;
   const imgWrapper = `w-[18.75rem] h-[18.75rem] relative`;
+  const infoIconStyle = `w-[3.125rem] h-[3.125rem] z-10 absolute right-2 top-2`;
   const imgStyle = `w-[18.75rem] h-[18.75rem] rounded-[20px_20px_20px_20px]`;
   const titleStyle = `text-2xl font-semibold text-text-color`;
   const textStyle = `text-xl font-normal text-text-color`;
@@ -15,6 +16,9 @@ const DestinationCard = ({ data }) => {
       {data?.map((item, index) => (
         <li key={index} className={gridItem}>
           <div className={imgWrapper}>
+            <Link href={`/details/${item.id}`}>
+              <Image src="/Assets/Info-icon.svg" alt="Info" className={infoIconStyle} width="50" height="50" />
+            </Link>
             <Image
               src={`http://localhost:8000${item.attributes.image}`}
               alt={item.attributes.title}
