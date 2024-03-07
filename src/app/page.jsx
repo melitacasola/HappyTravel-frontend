1b4ca7dfa3be4a1188e22bce42ee85fa472e4ecc
melitacasola <melissacasola@gmail.com>
2024-03-07 14:12:53 +0100

    feat: image fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,14 +4,11 @@ import Destinations from "./components/DestinationCard/Destinations";
 
 export default function Home() {
 
-
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <h1>Destinos</h1>
         <Destinations />
-       
       </div>
     </main>
   );
