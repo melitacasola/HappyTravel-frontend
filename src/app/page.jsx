abca0ecc52d8613ce53f368f6ca31b2689ac05a4
GreciaLH <glandazuriherrera@gmail.com>
2024-03-12 10:39:25 +0100

    Merge changes from dev into feature-cardbuttons

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,57 +1,13 @@
-"use client";
-import React, { useState, useEffect } from "react";
 import Destinations from "./components/DestinationCard/Destinations";
-import PaginationButtons from "./components/PaginationButtons/PaginationButtons";
-import { getDestinations } from "./services/axios";
 
-export default function Home({searchParams}) {
-  const query = searchParams?.query;
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  const [destinations, setDestinations] = useState([]);
-  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async (page) => {
-      try {
-        setLoading(true);
-        const response = await getDestinations(page);
-        setDestinations(response.data);
-        setTotalPages(response.meta.last_page);
-        setLoading(false);
-        
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData(currentPage); 
-  }, [currentPage]);
-
-  const handlePageUpdate = async (page) => {
-    setCurrentPage(page);
-    
-    const response = await getDestinations(currentPage);
-    setDestinations(response.data);
-  };
-
+export default function Home({ searchParams }) {
+  const query = searchParams?.query
+ 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-14">
       <div>
-        {loading ? (
-          <p>Cargando...</p>
-        ) : (
-          <>
-            <Destinations destinations={destinations} query={query} />
-
-            <div className="hidden md:block">
-              <PaginationButtons
-                currentPage={currentPage}
-                totalPages={totalPages}
-                updatePage={handlePageUpdate}
-              />
-            </div>
-          </>
-        )}
+        <Destinations query={query}/>
       </div>
     </main>
   );
